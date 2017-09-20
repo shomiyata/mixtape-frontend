c64e7be1b4878881704700f2586dca307e23d17e
shomiyata <eighteighttwenty@gmail.com>
2017-09-19 22:27:25 -0400

    added link to test api

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <a href="http://localhost:3000/api/v1/login">Click it</a>
+      </div>
+    )
+  }
+}
+
+export default App;
